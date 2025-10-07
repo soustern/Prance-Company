fa2a2b2e4875c5f76413770a5161d2b0db375110
soustern <rafaelantoniassivicechio@gmail.com>
2025-10-07 15:22:12 -0300

    Install important packages

---
+++ src/layout/NavBar.tsx
@@ -0,0 +1,18 @@
+import type { JSX } from "react";
+
+const NavBar = (): JSX.Element => {
+    
+    if ()
+    {
+        
+    }
+    else
+    {
+
+    }
+    return (
+        <></>
+    )
+}
+
+export default NavBar;
