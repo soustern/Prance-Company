39cfd402b3a4f4e66cba766d35049d2358d13dc7
soustern <rafaelantoniassivicechio@gmail.com>
2025-10-08 16:26:29 -0300

    Begin transition to about section

--- src/layout/About.tsx
+++ src/layout/About.tsx
@@ -1,8 +1,25 @@
 import type { JSX } from "react"
+import about from "../assets/about.png";
 
 const About = (): JSX.Element => {
     return (
-        <></>
+        <section className="bg-[var(--color-bg-primary)] flex flex-col items-center justify-center relative px-4 pt-20">
+            <div className="absolute flex left-1/2 -top-10 transform -translate-x-1/2 z-0 gap-0">
+                <div className="bg-slate-50 z-30 h-10 w-10 relative transform translate-x-1.5">
+                    <div className="bg-[var(--color-bg-primary)] h-full w-full rounded-br-full absolute inset-0"></div>
+                </div>
+                <div className="bg-slate-50 z-30 h-10 w-13 rounded-t-full relative"></div>
+                <div className="bg-slate-50 z-30 h-10 w-10 relative transform -translate-x-1.5">
+                    <div className="bg-[var(--color-bg-primary)] h-full w-full rounded-bl-full absolute inset-0 origin-left"></div>
+                </div>
+            </div>
+            <div className="absolute z-0 inset-0 bg-slate-50 w-full h-full transform origin-center scale-x-80 rounded-2xl"></div>
+            <div className="relative z-10 pb-6">
+                <img src={about} className="max-w-[250px] relative z-10" alt="" />
+                <img src={about} className="max-w-[250px] absolute inset-0 z-10 blur-xs" alt="" />
+            </div>
+            <h2 className="relative z-10 text-slate-800 text-normal text-center text-2xl leading-tight pb-4">Mais que Agência, <br></br> sua Consultoria Estratégica.</h2>
+        </section>
     )
 }
 
--- src/layout/hero.tsx
+++ src/layout/hero.tsx
@@ -11,13 +11,13 @@ const Hero = (): JSX.Element => {
     const size = useWindowSize();
 
     return (
-        <section className="relative z-0 min-h-[430px] flex flex-col items-center  bg-amber-300 px-4 pt-40">
+        <section className="relative z-0 min-h-[480px] flex flex-col items-center  bg-amber-300 px-4 pt-40">
             <div className="absolute z-0 inset-0 flex items-center justify-center h-full pt-20">
                 { size <= 700 ?<img src={heroMobile} alt="" className="object-cover w-full h-full" /> : <img src="src\assets\heroDesktop.webp" alt="" /> }
                 <div className="absolute z-0 inset-0 bg-[var(--color-bg-primary)]/98 backdrop-blur-sm mix-blend-multiply"></div>
-                <div className="absolute inset-0 z-0 bg-gradient-to-t from-[var(--color-bg-primary)] from-10% to-transparent to-100%"></div>
+                <div className="absolute inset-0 z-0 bg-gradient-to-t from-[var(--color-bg-primary)] from-8% to-transparent to-70%"></div>
             </div>
-            <h1 className="relative z-10 text-3xl font-normal text-center text-slate-400 pb-6">Do <span className="text-slate-50 font-medium">conceito</span> ao <span className="text-slate-50 font-medium">lucro</span> <br></br> com <span className="text-slate-50 font-medium">expertise</span></h1>
+            <h1 className="relative z-10 text-3xl text-center text-slate-400 pb-6">Do <span className="text-slate-50 font-medium">conceito</span> ao <span className="text-slate-50 font-medium">lucro</span> <br></br> com <span className="text-slate-50 font-medium">expertise</span></h1>
             <p className="relative z-10 text-lg font-normal text-center text-slate-400 pb-4">Cresça com clareza e impacto.</p>
             <div className="relatice z-10">
                 <SecondaryButton className="border-[var(--color-accent-secondary)] text-slate-200 text-5xl bg-[var(--color-bg-primary)]" text="Explore as possibilidades"><i className="fa-solid fa-star text-xl text-[var(--color-accent-secondary)]"></i></SecondaryButton>
