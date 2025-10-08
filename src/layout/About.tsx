b13550da59ab6d91685ac50903306d7ca5f94a8e
soustern <rafaelantoniassivicechio@gmail.com>
2025-10-08 16:33:51 -0300

    Create top tumor about section

--- src/layout/About.tsx
+++ src/layout/About.tsx
@@ -5,13 +5,14 @@ const About = (): JSX.Element => {
     return (
         <section className="bg-[var(--color-bg-primary)] flex flex-col items-center justify-center relative px-4 pt-20">
             <div className="absolute flex left-1/2 -top-10 transform -translate-x-1/2 z-0 gap-0">
-                <div className="bg-slate-50 z-30 h-10 w-10 relative transform translate-x-1.5">
+                <div className="bg-slate-50 z-10 h-10 w-10 relative transform translate-x-1.5">
                     <div className="bg-[var(--color-bg-primary)] h-full w-full rounded-br-full absolute inset-0"></div>
                 </div>
                 <div className="bg-slate-50 z-30 h-10 w-13 rounded-t-full relative"></div>
-                <div className="bg-slate-50 z-30 h-10 w-10 relative transform -translate-x-1.5">
-                    <div className="bg-[var(--color-bg-primary)] h-full w-full rounded-bl-full absolute inset-0 origin-left"></div>
+                <div className="bg-slate-50 z-10 h-10 w-10 relative transform -translate-x-1.5">
+                    <div className="bg-[var(--color-bg-primary)] h-full w-full rounded-bl-full absolute inset-0 "></div>
                 </div>
+                <div className="bg-[var(--color-bg-primary)] z-20 h-5 w-35 absolute inset-0"></div>
             </div>
             <div className="absolute z-0 inset-0 bg-slate-50 w-full h-full transform origin-center scale-x-80 rounded-2xl"></div>
             <div className="relative z-10 pb-6">
