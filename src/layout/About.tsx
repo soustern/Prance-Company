74bdfe16dcfc5ba95e7e888b042840a2ef306e5b
soustern <rafaelantoniassivicechio@gmail.com>
2025-10-10 16:13:53 -0300

    Complete About section animations

--- src/layout/About.tsx
+++ src/layout/About.tsx
@@ -1,10 +1,11 @@
-import { useRef, type JSX } from "react"
+import { use, useRef, type JSX } from "react"
 import about from "../assets/about.png";
 import { useGSAP } from "@gsap/react";
 import logoAbout from "../assets/logoAbout.webp";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
+import PrimaryButton from "../components/PrimaryButton";
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(SplitText);
@@ -19,6 +20,8 @@ const About = (): JSX.Element => {
     const imageRef = useRef<HTMLImageElement>(null);
     const imageBlurRef = useRef<HTMLImageElement>(null);
     const aboutHeading = useRef<HTMLHeadingElement>(null);
+    const aboutParagraph = useRef<HTMLParagraphElement>(null);
+    const buttonRef = useRef<HTMLDivElement>(null);
 
     useGSAP(() => {
         gsap.set(logoRef.current, { opacity: 0 });
@@ -98,10 +101,37 @@ const About = (): JSX.Element => {
             }
         })
 
+        gsap.from(aboutParagraph.current, {
+            opacity: 0,
+            duration: 0.5,
+            scale: 0.8,
+            ease: "power4.out",
+            scrollTrigger: {
+                trigger: imageRef.current,
+                start: "center center",
+                end: "bottom center",
+                scrub: true,
+            }
+        })
+
+        gsap.from(buttonRef.current, {
+            y: 100,
+            opacity: 0,
+            stagger: 0.05,
+            ease: "power4.out",
+            duration: 0.2,
+            scrollTrigger: {
+                trigger: aboutParagraph.current,
+                start: "center bottom",
+                end: "+=400 bottom",
+                scrub: true,
+            }
+        })
+
     }, []);
 
     return (
-        <section className="bg-[var(--color-bg-primary)] flex flex-col items-center justify-center relative px-4 pt-8 pb-60">
+        <section className="bg-[var(--color-bg-primary)] flex flex-col items-center justify-center relative px-4 py-8">
             <div className="absolute flex left-1/2 -top-10 transform -translate-x-1/2 z-0 gap-0">
                 <div className="bg-slate-100 z-10 h-10 w-10 relative transform translate-x-1.5">
                     <div className="bg-[var(--color-bg-primary)] h-full w-full rounded-br-full absolute inset-0"></div>
@@ -116,11 +146,15 @@ const About = (): JSX.Element => {
                 <div className="bg-[var(--color-bg-primary)] z-20 h-5 w-35 absolute inset-0"></div>
             </div>
             <div ref={backgroundRef} className="absolute z-0 inset-0 bg-slate-100 w-full h-full transform origin-center scale-x-80 rounded-4xl"></div>
-            <div className="relative z-10 pb-6">
+            <div className="relative z-10 pb-8">
                 <img src={about} ref={imageRef} className="max-w-[280px] relative z-10" alt="" />
                 <img src={about} ref={imageBlurRef} className="max-w-[280px] absolute inset-0 z-10 blur-xs" alt="" />
             </div>
             <h2 ref={aboutHeading} className="relative z-10 text-[var(--color-bg-primary)] font-bolds text-center text-2xl leading-tight pb-4">Mais que Agência, <br></br> sua Consultoria Estratégica!</h2>
+            <p ref={aboutParagraph} className="relative z-10 text-center font-light color-[var(--color-bg-primary)] pb-4">Consultoria de marketing digital e branding que transforma a autoridade de empresas em resultados reais através de planejamento, execução e acompanhamento estratégico.</p>
+            <div className="w-full" ref={buttonRef}>
+                <PrimaryButton text="Vamos conversar"></PrimaryButton>
+            </div>
         </section>
     )
 }
