d468cf7da5d9f414180c4e1351deeebc89ef6367
soustern <rafaelantoniassivicechio@gmail.com>
2025-10-11 21:45:31 -0300

    Add shadow to card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ interface cardProps {
 
 const Card = ({heading, paragraph, icon}: cardProps): JSX.Element => {
     return (
-        <article className="bg-slate-200 rounded-4xl px-8 py-8">
+        <article className="bg-slate-200 rounded-4xl px-8 py-8 shadow-xl">
             <div className="flex gap-2 items-center justify-center pb-4">
                 <h3 className="text-2xl leading-tight  font-medium text-center text-slate-800">{heading}</h3>
                 <i className={`fa-solid fa-${icon} text-2xl text-[var(--color-accent-secondary)]`}></i>
