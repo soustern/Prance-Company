cf7b7cad7eaef5b08abbb4738ea1f43bb9afa534
soustern <rafaelantoniassivicechio@gmail.com>
2025-10-14 17:19:24 -0300

    Finish Services Section

--- src/layout/Services.tsx
+++ src/layout/Services.tsx
@@ -4,6 +4,8 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 import Card from "../components/Card";
+import servicesOwner from "../assets/servicesOwner.webp";
+import servicesBackground from "../assets/servicesBackground.webp";
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(SplitText);
@@ -11,13 +13,21 @@ gsap.registerPlugin(SplitText);
 const Services = (): JSX.Element => {
     const servicesHeading = useRef<HTMLHeadingElement>(null);
     const servicesParagraph = useRef<HTMLParagraphElement>(null);
+    const servicesSecondHeading = useRef<HTMLHeadingElement>(null);
+    const servicesSecondParagraph = useRef<HTMLParagraphElement>(null);
+    
     const sectionRef = useRef<HTMLElement>(null);
     const firstCardRef = useRef<HTMLElement>(null);
     const secondCardRef = useRef<HTMLElement>(null);
     const thirdCardRef = useRef<HTMLElement>(null);
+    const imageRef = useRef<HTMLImageElement>(null);
+    const imageBlurRef = useRef<HTMLImageElement>(null);
 
     useGSAP(() => {
+        gsap.set(imageRef.current, { scale: 0.8, force3D: true });
+        gsap.set(imageBlurRef.current, { scale: 0.8, force3D: true });
         const split = new SplitText(servicesHeading.current, {type: "lines"});
+        
         gsap.from(split.lines, {
             y: 100,
             opacity: 0,
@@ -68,6 +78,65 @@ const Services = (): JSX.Element => {
             duration: 0.2,
         })
 
+        gsap.to(imageBlurRef.current, {
+            scaleX: 1, 
+            opacity: 0, 
+            stagger: 0.05,
+            willChange: "transform, opacity",
+            ease: "power4.out",
+            scrollTrigger: {
+                trigger: sectionRef.current,
+                start: "80% center",
+                end: "85% center",
+                scrub: 1,
+                toggleActions: "play none none reverse",
+            }
+        });
+
+        gsap.to(imageRef.current, {
+            scale: 1,
+            stagger: 0.05,
+            ease: "power4.out",
+            willChange: "transform",
+            scrollTrigger: {
+                trigger: sectionRef.current,
+                start: "80% center",
+                end: "85% center",
+                scrub: 1,
+                toggleActions: "play none none reverse",
+            }
+        });
+
+         gsap.from(servicesSecondHeading.current, {
+            y: 100,
+            opacity: 0,
+            stagger: 0.05,
+            ease: "power4.out",
+            willChange: "transform, opacity",
+            scrollTrigger: {
+                trigger: sectionRef.current,
+                start: "87% center",
+                end: "91% center",
+                scrub: 1,
+                toggleActions: "play none none reverse",
+            }
+        })
+
+        gsap.from(servicesSecondParagraph.current, {
+            opacity: 0,
+            stagger: 0.05,
+            scale: 0.8,
+            willChange: "transform, opacity",
+            ease: "power4.out",
+            scrollTrigger: {
+                trigger: sectionRef.current,
+                start: "92% center",
+                end: "95% center",
+                scrub: 1,
+                toggleActions: "play none none reverse",
+            }
+        })
+
 
         if (document.readyState === "complete")
         {
@@ -94,16 +163,22 @@ const Services = (): JSX.Element => {
     });
 
     return (
-        <section ref={sectionRef} className="px-4 py-8 bg-slate-800">
-            <h2 ref={servicesHeading} className="[will-change: opacity, transform] font-medium text-center text-2xl leading-tight pb-4 text-slate-200">3 pilares essenciais para <br></br> escalar sua marca</h2>
-            <p ref={servicesParagraph} className="[will-change: opacity, transform] text-center font-light pb-8 text-slate-300">Não é sorte, é estratégia. Toda marca de sucesso segue uma base sólida. Nossos 3 pilares mostram o caminho para construir autoridade, gerar conexão real e escalar resultados no digital.</p>
-            <div className="relative min-h-[200vh] cards flex flex-col items-center justify-baseline">
+    <section ref={sectionRef} style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.6)), url(${servicesBackground})`}} className="  bg-bottom bg-no-repeat z-0 px-4 py-8 bg-slate-800 flex flex-col items-center relative">
+            <h2 ref={servicesHeading} className="[will-change: opacity, transform] font-medium text-center text-2xl leading-tight pb-4 text-slate-200 relative z-10">3 pilares essenciais para <br></br> escalar sua marca</h2>
+            <p ref={servicesParagraph} className="[will-change: opacity, transform] text-center font-light pb-8 text-slate-300 relative z-10">Não é sorte, é estratégia. Toda marca de sucesso segue uma base sólida. Nossos 3 pilares mostram o caminho para construir autoridade, gerar conexão real e escalar resultados no digital.</p>
+            <div className="relative min-h-[200vh] cards flex flex-col items-center justify-baseline mb-8 z-10">
                 <div className="card-wrapper h-full flex flex-col items-center justify-baseline gap-4">
                     <Card ref={firstCardRef} className="z-10 card" heading="Branding & Design" paragraph="O segredo para uma marca irresistível começa aqui. Identidade, posicionamento e estética pensados para gerar confiança imediata e conquistar espaço na mente do seu público." icon="pen-nib"></Card>
                     <Card ref={secondCardRef} className="z-20 card" heading="Conteúdo & Social Media" paragraph="O motor que mantém sua marca em movimento. Estratégias de crescimento aliadas a conteúdo estratégico que conecta, engaja e cria um público fiel." icon="photo-film"></Card>
                     <Card ref={thirdCardRef} className="z-30 card transform -translate-y-[90%]" heading="Mídia Paga & Performance" paragraph="Não basta ser visto, é preciso converter. Estruturamos campanhas inteligentes que transformam atenção em vendas, com métricas sólidas e foco total em resultados." icon="rocket"></Card>
                 </div>
             </div>
+            <div className="relative z-10 pb-8 flex items-center">
+                <img loading="lazy" src={servicesOwner} ref={imageRef} className="[will-change: transform, opacity] max-w-[280px] relative z-10" alt="" />
+                <img loading="lazy" src={servicesOwner} ref={imageBlurRef} className="[will-change: transform, opacity] max-w-[280px] absolute inset-0 z-10 blur-xs" alt="" />
+            </div>
+            <h2 ref={servicesSecondHeading}  className="[will-change: opacity, transform] font-medium text-center text-2xl leading-tight pb-4 text-slate-200 relative z-10">Conheça nossa FUNDADORA</h2>
+            <p ref={servicesSecondParagraph}  className="[will-change: opacity, transform] text-center font-light pb-8 text-slate-300 relative z-10">Priscila Pavanette é publicitária especialista em campanhas digitais e gestão de mídia. Liderou projetos para marcas como FAJ Empreendimentos, UNIRP, WebPic, Energy Field, Tyson Burger, Savannah Brand e Luzia Fazzolli, gerando resultados consistentes. Fundou a Prance Company para transformar marcas em referências no mercado.</p>
         </section>
     )
 }
