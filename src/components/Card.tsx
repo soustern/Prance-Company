98ab4a6ec46cc9b7ab999cc00c1496696e21d850
soustern <rafaelantoniassivicechio@gmail.com>
2025-10-20 17:57:03 -0300

    Change cards design

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,18 +8,21 @@ interface cardProps {
     className?: string
 }
 
-// TODO: Add Prop for button onClick
 // TODO: Fix Icon Positioning
 
 const Card = forwardRef<HTMLElement, cardProps>(({heading, paragraph, icon, className}, ref): JSX.Element => {
     return (
-        <article ref={ref} className={`bg-slate-200 rounded-4xl px-8 py-8 shadow-xl border border-[var(--color-accent-secondary)] max-w-[320px] ${className} pointer-events-auto`}>
-            <div className="flex gap-2 items-center justify-center pb-4">
+        <article ref={ref} className={`overflow-hidden bg-slate-200 rounded-4xl pb-8 shadow-xl border border-slate-200 max-w-[320px] ${className} pointer-events-auto`}>
+            <div className="flex flex-col gap-8 items-center justify-center pb-4">
+                <div className="bg-bg-primary w-full h-full py-6 flex items center justify-center">
+                    {icon && <i className={`fa-solid fa-${icon} text-4xl text-accent-secondary`}></i>}
+                </div>
                 <h3 className="text-2xl leading-tight  font-medium text-center text-slate-800">{heading}</h3>
-                {icon && <i className={`fa-solid fa-${icon} text-2xl text-[var(--color-accent-secondary)]`}></i>}
             </div> 
-            <p className="text-slate-700 text-center font-light pb-8">{paragraph}</p>
-            <PrimaryButton link="https://wa.link/173tl9" className="pointer-events-auto relative z-50" text="Saiba Mais"></PrimaryButton>
+            <div className="px-8">
+                <p className="text-slate-700 text-center font-light pb-8">{paragraph}</p>
+                <PrimaryButton link="https://wa.link/173tl9" className="pointer-events-auto relative z-50" text="Saiba Mais"></PrimaryButton>
+            </div>
         </article> 
     )
 })
