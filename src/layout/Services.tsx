35c6765e42b408ac26b88cf51431169c1d4052e3
soustern <rafaelantoniassivicechio@gmail.com>
2025-10-21 16:40:55 -0300

    Fix background gradient

--- src/layout/Services.tsx
+++ src/layout/Services.tsx
@@ -172,7 +172,7 @@ const Services = ({fontsReady}: servicesProps): JSX.Element => {
 
 
     return (
-    <section id="services-section" ref={sectionRef} style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.6)), url(${servicesBackground})`}} className="  bg-bottom bg-no-repeat z-0 px-4 py-8 bg-slate-800 flex flex-col items-center relative scroll-mt-20">
+    <section id="services-section" ref={sectionRef} style={{backgroundImage: `linear-gradient(rgba(15, 23, 43, 0.9), rgba(15, 23, 43, 0.6)), url(${servicesBackground})`}} className="  bg-bottom bg-no-repeat z-0 px-4 py-8  flex flex-col items-center relative scroll-mt-20">
             <h2 ref={servicesHeading} className="[will-change: opacity, transform] font-medium text-center text-2xl leading-tight pb-4 text-slate-200 relative z-10">3 pilares essenciais para <br></br> escalar sua marca</h2>
             <p ref={servicesParagraph} className="[will-change: opacity, transform] text-center font-light pb-8 text-slate-300 relative z-10">Não é sorte, é estratégia. Toda marca de sucesso segue uma base sólida. Nossos 3 pilares mostram o caminho para construir autoridade, gerar conexão real e escalar resultados no digital.</p>
             <div className="relative cards flex flex-col mb-8 z-10">
