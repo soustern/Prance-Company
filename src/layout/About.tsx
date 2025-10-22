62c2669f784bcf9c8ecde3666eb9c803aa1a7f5b
soustern <rafaelantoniassivicechio@gmail.com>
2025-10-22 18:50:41 -0300

    Begin ScrollTrigger refactor

--- src/layout/About.tsx
+++ src/layout/About.tsx
@@ -7,6 +7,7 @@ import { SplitText } from "gsap/SplitText";
 import PrimaryButton from "../components/PrimaryButton";
 import aboutUiElement from "../assets/aboutUiElement.svg";
 import logoAbout from "../assets/logoAbout.webp";
+import { useWindowSize } from "../hooks/useWindowSize";
 
 
 gsap.registerPlugin(ScrollTrigger);
@@ -20,6 +21,8 @@ interface aboutProps {
 
 
 const About = ({fontsReady}: aboutProps): JSX.Element => {
+    const size = useWindowSize();
+
     const iconRef = useRef<HTMLElement>(null);
     const logoRef = useRef<HTMLImageElement>(null);
     const backgroundRef = useRef<HTMLDivElement>(null);
@@ -29,6 +32,12 @@ const About = ({fontsReady}: aboutProps): JSX.Element => {
     const aboutParagraph = useRef<HTMLParagraphElement>(null);
     const buttonRef = useRef<HTMLDivElement>(null);
 
+    const headingDesktopRef = useRef<HTMLHeadingElement>(null);
+    const paragraphDesktopRef = useRef<HTMLParagraphElement>(null);
+    const buttonDesktopRef = useRef<HTMLDivElement>(null);
+    const imageDesktopRef = useRef<HTMLImageElement>(null);
+    const sectionDesktopRef = useRef<HTMLElement>(null);
+
     useGSAP(() => {
         if (!fontsReady) return;
 
@@ -36,6 +45,10 @@ const About = ({fontsReady}: aboutProps): JSX.Element => {
         if (refs.some(ref => !ref.current)) return;
 
         const split = SplitText.create(aboutHeading.current, {
+            type: "lines"
+        });
+
+        const splitParagraph = SplitText.create(aboutParagraph.current, {
             type: "lines"
         });
 
@@ -132,12 +145,13 @@ const About = ({fontsReady}: aboutProps): JSX.Element => {
                 }
             })
 
-            gsap.from(aboutParagraph.current, {
+            gsap.from(splitParagraph.lines, {
+                y: 100,
                 opacity: 0,
-                duration: 0.5,
-                scale: 0.8,
-                willChange: "transform, opacity",
+                stagger: 0.05,
                 ease: "power4.out",
+                willChange: "transform, opacity",
+                duration: 0.2,
                 scrollTrigger: {
                     trigger: imageRef.current,
                     start: "center center",
@@ -154,9 +168,9 @@ const About = ({fontsReady}: aboutProps): JSX.Element => {
                 duration: 0.2,
                 willChange: "transform, opacity",
                 scrollTrigger: {
-                    trigger: aboutParagraph.current,
-                    start: "center bottom",
-                    end: "+=400 bottom",
+                    trigger: backgroundRef.current,
+                    start: "bottom bottom",
+                    end: "120% bottom",
                     scrub: 1,
                 }
             })
@@ -190,32 +204,123 @@ const About = ({fontsReady}: aboutProps): JSX.Element => {
             
             ScrollTrigger.getAll().forEach(trigger => trigger.kill()); 
             split.revert();
+            splitParagraph.revert();
             document.removeEventListener("load", handleLoad);
         };
 
     }, {dependencies: [fontsReady]});
 
-    return (
-        <section id="about-section" className="bg-[var(--color-bg-primary)] flex flex-col items-center justify-center relative px-10 py-16 scroll-mt-15">
-            <div className="absolute flex left-1/2 -top-16 transform -translate-x-1/2 translate-y-[1px] z-10 gap-0 ">
-                <img src={aboutUiElement} alt="" className="w-22" />
-                <i  ref={iconRef} className="[will-change: opacity] fa-solid fa-arrow-down text-2xl text-[var(--color-bg-primary)] animate-bounce absolute top-[60%] left-1/2 transform -translate-x-1/2 -translate-y-1/2"></i>
-                <img ref={logoRef} src={logoAbout} className="[will-change: opacity] w-8 absolute top-[50%] left-1/2 transform -translate-x-1/2 -translate-y-1/2"></img>
-            </div>
-            <div ref={backgroundRef} className="[will-change: transform] absolute z-0 inset-0 bg-slate-200 w-full h-full transform origin-center scale-x-80 rounded-t-4xl"></div>
-            <div className="relative z-10 pb-8">
-                <img  src={about} ref={imageRef} className="[will-change: transform, opacity] max-w-[300px] relative z-10" alt="" />
-                <img  src={about} ref={imageBlurRef} className="[will-change: transform, opacity] max-w-[300px] absolute inset-0 z-10 blur-xs" alt="" />
-            </div>
-            <div>
-                <h2 ref={aboutHeading} className="[will-change: transform, opacity] relative z-10 text-[var(--color-bg-primary)] font-medium text-2xl leading-tight pb-4">Mais que Agência, <br></br> sua Consultoria Estratégica!</h2>
-                <p ref={aboutParagraph} className="[will-change: transform, opacity] relative z-10 font-light text-[var(--color-bg-primary)] pb-4">Consultoria de marketing digital e branding que transforma a autoridade de empresas em resultados reais através de planejamento, execução e acompanhamento estratégico.</p>
-            </div>
-            <div className="w-full [will-change: transform, opacity] relative z-50" ref={buttonRef}>
-                <PrimaryButton text="Vamos conversar" link="https://wa.link/173tl9" className="pointer-events-auto" ></PrimaryButton>
-            </div>
-        </section>
-    )
+    useGSAP(() => {
+        if (!fontsReady) return;
+
+        const refs = [headingDesktopRef, paragraphDesktopRef, buttonDesktopRef, imageDesktopRef, sectionDesktopRef];
+        if (refs.some(ref => !ref.current)) return;
+
+        const splitHeading = SplitText.create(headingDesktopRef.current, {
+            type: "lines"
+        });
+
+        const splitParagraph = SplitText.create(paragraphDesktopRef.current, {
+            type: "lines"
+        });
+
+        let isInitialized = false;
+        const initAnimations = () => {
+            if (isInitialized) return;
+            isInitialized = true;
+
+            gsap.from(splitHeading.lines, {
+                y: 100,
+                opacity: 0,
+                stagger: 0.05,
+                ease: "power4.out",
+                force3D: true,
+                duration: 0.2,
+                scrollTrigger: {
+                    trigger: sectionDesktopRef.current,
+                    start: "40% bottom",
+                    end: "60% bottom",
+                    scrub: 1,
+                }
+            });
+
+            gsap.from(splitParagraph.lines, {
+                y: 100,
+                opacity: 0,
+                stagger: 0.05,
+                ease: "power4.out",
+                force3D: true,
+                duration: 0.2,
+                scrollTrigger: {
+                    trigger: sectionDesktopRef.current,
+                    start: "50% bottom",
+                    end: "90% bottom",
+                    scrub: 1,
+                }
+            });
+        }
+
+        const initTimeout = setTimeout(initAnimations, 50);
+
+        const onload = () => ScrollTrigger.refresh();
+        window.addEventListener("load", onload);
+
+        return () => {
+            splitHeading.revert();
+            splitParagraph.revert();
+            isInitialized = false;
+            clearTimeout(initTimeout);
+            window.removeEventListener("load", onload);
+            ScrollTrigger.killAll();
+        };
+
+    }, {dependencies: [fontsReady]});
+
+    if (size <= 975)
+    {
+        return (
+            <section id="about-section" className="bg-[var(--color-bg-primary)] flex flex-col items-center justify-center relative px-10 py-16 scroll-mt-15">
+                <div className="absolute flex left-1/2 -top-16 transform -translate-x-1/2 translate-y-[1px] z-10 gap-0 ">
+                    <img src={aboutUiElement} alt="" className="w-22" />
+                    <i  ref={iconRef} className="[will-change: opacity] fa-solid fa-arrow-down text-2xl text-[var(--color-bg-primary)] animate-bounce absolute top-[60%] left-1/2 transform -translate-x-1/2 -translate-y-1/2"></i>
+                    <img ref={logoRef} src={logoAbout} className="[will-change: opacity] w-8 absolute top-[50%] left-1/2 transform -translate-x-1/2 -translate-y-1/2"></img>
+                </div>
+                <div ref={backgroundRef} className="[will-change: transform] absolute z-0 inset-0 bg-slate-200 w-full h-full transform origin-center scale-x-80 rounded-t-4xl"></div>
+                <div className="relative z-10 pb-8">
+                    <img  src={about} ref={imageRef} className="[will-change: transform, opacity] max-w-[300px] relative z-10" alt="" />
+                    <img  src={about} ref={imageBlurRef} className="[will-change: transform, opacity] max-w-[300px] absolute inset-0 z-10 blur-xs" alt="" />
+                </div>
+                <div>
+                    <h2 ref={aboutHeading} className="[will-change: transform, opacity] relative z-10 text-slate-800 font-medium text-2xl leading-tight pb-4">Mais que Agência, <br></br> sua Consultoria Estratégica!</h2>
+                    <p ref={aboutParagraph} className="[will-change: transform, opacity] relative z-10 font-light text-slate-700 pb-4">Consultoria de marketing digital e branding que transforma a autoridade de empresas em resultados reais através de planejamento, execução e acompanhamento estratégico.</p>
+                </div>
+                <div className="w-full [will-change: transform, opacity] relative z-50" ref={buttonRef}>
+                    <PrimaryButton text="Vamos conversar" link="https://wa.link/173tl9" className="pointer-events-auto" ></PrimaryButton>
+                </div>
+            </section>
+        )
+    }
+    else {
+        return (
+            <section ref={sectionDesktopRef} id="about-section" className="py-50 z-0 flex items-center justify-center px-16 relative bg-slate-200">
+                <div className="w-full max-w-[1200px] grid grid-cols-2 gap-16 items-center">
+                    <div className="relative z-20 flex items-center justify-center">
+                        <img ref={imageDesktopRef} src={about} alt="" className="w-[500px]" />
+                    </div>
+                    <div className="relative z-20">
+                        <h1 ref={headingDesktopRef} className="relative z-10 text-4xl text-slate-800 pb-8 font-light">Mais que Agência, <br></br> sua
+                        Consultoria Estratégica.</h1>
+                        <p ref={paragraphDesktopRef} className=" text-slate-700 pb-12 relative z-10 text-xl font-extralight leading-relaxed">Somos uma consultoria especializada em fornecer serviços de alto nível em marketing digital e branding para empresas que visam melhorar seu posicionamento e alcançar novos voos.
+                        <br></br><br></br>Planejamento completo, execução inteligente e acompanhamento contínuo para transformar autoridade em resultados reais.</p>
+                        <div ref={buttonDesktopRef}>
+                            <PrimaryButton text="Vamos conversar" link="https://wa.link/173tl9" className="max-w-[250px]" ></PrimaryButton>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+    
 }
 
 export default About;
--- src/layout/hero.tsx
+++ src/layout/hero.tsx
@@ -38,6 +38,7 @@ const Hero = ({fontsReady}: heroProps): JSX.Element => {
         if (refs.some(ref => !ref.current)) return;
 
         const splitHeading = new SplitText(headingRef.current, {type: "lines"});
+        const splitParagraph = new SplitText(paragraphRef.current, {type: "lines"});
 
         let isInitialized = false;
 
@@ -49,31 +50,30 @@ const Hero = ({fontsReady}: heroProps): JSX.Element => {
             gsap.from(splitHeading.lines, {
                 y: 100,
                 opacity: 0,
-                stagger: 0.08,
                 ease: "power4.out",
                 willChange: "transform, opacity",
                 delay: 2.5,
                 duration: 1.1,
                 });
 
-            gsap.from(paragraphRef.current, {
-                opacity: 0,
+            gsap.from(splitParagraph.lines, {
+                y: 100,
+                stagger: 0.08,
                 ease: "power4.out",
                 willChange: "transform, opacity",
-                delay: 2.7,
+                delay: 2.8,
                 duration: 1.1,
-                scale: 0.8,
                 });
 
             gsap.from(buttonRef.current, {
-                y: 20,
+                y: 100,
                 opacity: 0,
                 ease: "power4.out",
                 willChange: "transform, opacity",
-                delay: 2.9,
+                delay: 3.2,
                 duration: 1.1,
                 });
-        }
+        };
 
         const initTimeout = setTimeout(initAnimations, 50);
 
@@ -81,6 +81,7 @@ const Hero = ({fontsReady}: heroProps): JSX.Element => {
             clearTimeout(initTimeout);
             isInitialized = false;
             splitHeading.revert();
+            splitParagraph.revert();
         };
     }, {dependencies: [fontsReady]});
 
@@ -91,6 +92,7 @@ const Hero = ({fontsReady}: heroProps): JSX.Element => {
         if (refs.some(ref => !ref.current)) return;
 
         const splitHeading = new SplitText(headingDesktopRef.current, {type: "lines"});
+        const splitParagraph = new SplitText(paragraphDesktopRef.current, {type: "lines"});
 
         let isInitialized = false;
 
@@ -109,21 +111,22 @@ const Hero = ({fontsReady}: heroProps): JSX.Element => {
                 duration: 1.1,
                 });
 
-            gsap.from(paragraphDesktopRef.current, {
+            gsap.from(splitParagraph.lines, {
+                y: 100,
                 opacity: 0,
+                stagger: 0.08,
                 ease: "power4.out",
                 willChange: "transform, opacity",
-                delay: 2.7,
+                delay: 2.8,
                 duration: 1.1,
-                scale: 0.8,
                 });
 
             gsap.from(buttonDesktopRef.current, {
-                y: 20,
+                y: 100,
                 opacity: 0,
                 ease: "power4.out",
                 willChange: "transform, opacity",
-                delay: 2.9,
+                delay: 3.2,
                 duration: 1.1,
                 });
         }
@@ -146,7 +149,7 @@ const Hero = ({fontsReady}: heroProps): JSX.Element => {
                     <div className="absolute inset-0 w-full h-full bg-gradient-to-t from-bg-primary from-30% to-bg-primary/85 bg-blend-hard-light"></div>
                 </div>
                 <div>
-                    <h1 ref={headingRef} className="relative z-10 text-3xl text-slate-400 pb-4"><span className="text-slate-50 font-medium">Do conceito ao lucro:</span> expertise que prepara sua marca para voar alto.</h1>
+                    <h1 ref={headingRef} className="relative z-10 text-3xl text-slate-200 pb-4"><span className="text-accent-secondary font-medium">Do conceito ao lucro:</span> expertise que prepara sua marca para voar alto.</h1>
                     <p ref={paragraphRef} className="text-lg font-normal text-slate-400 pb-8 relative z-10">Soluções em marketing digital, conteúdo estratégico, branding e soluções para negócios que querem crescer com clareza e impacto.</p>
                 </div>
                 <div ref={buttonRef} className="relative z-10 w-full">
@@ -160,17 +163,17 @@ const Hero = ({fontsReady}: heroProps): JSX.Element => {
             <section id="hero-section" className="h-[90vh] z-0 flex items-center justify-center px-16 relative">
                 <img src={heroDesktop} alt="" className="w-full h-full object-cover absolute inset-0 z-0" />
                 <div className="w-full h-full absolute inset-0 z-10 bg-gradient-to-r from-bg-primary from-25% to-70% to-transparent mix-blend-multiply"></div>
-                <div className="w-full max-w-[1200px] grid grid-cols-2 gap-16">
+                <div className="w-full max-w-[1200px] grid grid-cols-2 gap-16 relative z-10">
                     <div className="relative z-20">
-                        <h1 ref={headingDesktopRef} className="relative z-10 text-4xl text-slate-200 pb-8 font-light"><span className="text-accent-secondary font-medium">Do conceito ao lucro:</span> expertise que prepara sua marca para voar alto.</h1>
-                        <p ref={paragraphDesktopRef} className=" text-slate-200 pb-12 relative z-10 text-xl font-extralight leading-relaxed">Soluções em marketing digital, conteúdo estratégico, branding e soluções para negócios que querem crescer com clareza e impacto.</p>
+                        <h1 ref={headingDesktopRef} className="relative z-10 text-4xl text-slate-200 pb-8 font-light"><span className="text-accent-secondary font-medium">Do conceito ao lucro:</span><br></br> expertise que prepara sua marca para voar alto.</h1>
+                        <p ref={paragraphDesktopRef} className=" text-slate-400 pb-12 relative z-10 text-xl font-extralight leading-relaxed">Soluções em marketing digital, conteúdo estratégico, branding e soluções para negócios que querem crescer com clareza e impacto.</p>
                         <div ref={buttonDesktopRef}>
                             <SecondaryButton func={() => window.open("https://wa.link/173tl9", "_blank")} className="border-[var(--color-accent-secondary)] text-slate-200 text-lg bg-gradient-to-t from-slate-900 from-5% to-60% to-bg-primary max-w-[300px] transform hover:scale-101 hover:shadow-2xl transition-all" text="Vamos Conversar">
                                 <motion.i variants={{initial: {scale: 1, opacity: 0.7}, hover: {scale: 1.2, opacity: 1}}} transition={{ duration: 0.3, type: "spring", stiffness: 500, damping: 30 }}  className="fa-solid fa-star text-xl text-[var(--color-accent-secondary)]"></motion.i>
                             </SecondaryButton>
                         </div>
                     </div>
-                    <div className="relative z-20">ola</div>
+                    <div className="relative z-20"></div>
                 </div>
             </section> 
         )
